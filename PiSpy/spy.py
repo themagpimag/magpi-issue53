55e7eaa1232952b9f0c773fd6d5239cd1216fa27
Rob Zwetsloot <robertzwetsloot@gmail.com>
2016-12-14 11:00:05 +0000

    First commit

---
+++ PiSpy/spy.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from gpiozero import MotionSensor
+from picamera import PiCamera
+from datetime import datetime
+from time import sleep
+
+sensor = MotionSensor(14)
+camera = PiCamera()
+
+while True:
+    sensor.wait_for_motion()
+    filename = datetime.now().strftime("%H.%M.%S_%Y-%m-%d.jpg")
+    camera.capture(filename)
+    sleep(5)
